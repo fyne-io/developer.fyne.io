b383c237e99ad64ceaa513a394e7b296af41ba3a
Jacalz <jacob.alzen@gmail.com>
2020-06-12 13:35:06 +0200

    Update tour for deprectaion of fixed grid layout
    The fixed grid layout was deprecated and replaced with grid wrap layout as of Fyne version 1.3.0.
    This Pull Request updates the tour conent to take this into concideration.

--- _includes/tour/layout/gridwraplayout.go
+++ _includes/tour/layout/gridwraplayout.go
@@ -11,15 +11,15 @@ import (
 
 func main() {
 	myApp := app.New()
-	myWindow := myApp.NewWindow("Fixed Grid Layout")
+	myWindow := myApp.NewWindow("Grid Wrap Layout")
 
 	text1 := canvas.NewText("1", color.White)
 	text2 := canvas.NewText("2", color.White)
 	text3 := canvas.NewText("3", color.White)
-	grid := fyne.NewContainerWithLayout(layout.NewFixedGridLayout(fyne.NewSize(50, 50)),
+	grid := fyne.NewContainerWithLayout(layout.NewGridWrapLayout(fyne.NewSize(50, 50)),
 		text1, text2, text3)
 	myWindow.SetContent(grid)
 
-	//	myWindow.Resize(fyne.NewSize(180, 75))
+	// myWindow.Resize(fyne.NewSize(180, 75))
 	myWindow.ShowAndRun()
 }
