61367c3f2b59a14c9a898813a9b9419a5d80cc0a
Andy Williams <andy@andywilliams.me>
2021-03-01 18:33:30 +0000

    Add list to widget tour

---
+++ _includes/tour/widget/list.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+)
+
+var data = []string{"a", "string", "list"}
+
+func main() {
+	myApp := app.New()
+	myWindow := myApp.NewWindow("List Widget")
+
+	list := widget.NewList(
+		func() int {
+			return len(data)
+		},
+		func() fyne.CanvasObject {
+			return widget.NewLabel("template")
+		},
+		func(i widget.ListItemID, o fyne.CanvasObject) {
+			o.(*widget.Label).SetText(data[i])
+		})
+
+	myWindow.SetContent(list)
+	myWindow.ShowAndRun()
+}
