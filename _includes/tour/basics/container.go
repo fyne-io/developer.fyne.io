fc0438a0f096aac1d77f551702713befdd4d0954
Andy Williams <andy@andy.xyz>
2021-06-23 11:09:29 +0100

    Better contrast for container demo

--- _includes/tour/basics/container.go
+++ _includes/tour/basics/container.go
@@ -13,13 +13,13 @@ import (
 func main() {
 	myApp := app.New()
 	myWindow := myApp.NewWindow("Container")
+	green := color.NRGBA{R: 0, G: 180, B: 0, A: 255}
 
-	text1 := canvas.NewText("Hello", color.White)
-	text2 := canvas.NewText("There", color.White)
+	text1 := canvas.NewText("Hello", green)
+	text2 := canvas.NewText("There", green)
 	text2.Move(fyne.NewPos(20, 20))
-	text3 := canvas.NewText("World", color.White)
-	content := container.NewWithoutLayout(text1, text2, text3)
-	// content := container.New(layout.NewGridLayout(2), text1, text2, text3)
+	content := container.NewWithoutLayout(text1, text2)
+	// content := container.New(layout.NewGridLayout(2), text1, text2)
 
 	myWindow.SetContent(content)
 	myWindow.ShowAndRun()
