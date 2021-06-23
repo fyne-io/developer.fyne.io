b1851bea850459ccac531464b07c55f1ebd661a7
Andy Williams <andy@andy.xyz>
2021-06-23 11:49:11 +0100

    Fix colours to work in light or dark

--- _includes/tour/basics/canvas.go
+++ _includes/tour/basics/canvas.go
@@ -15,7 +15,8 @@ func main() {
 	myWindow := myApp.NewWindow("Canvas")
 	myCanvas := myWindow.Canvas()
 
-	text := canvas.NewText("Text", color.White)
+	green := color.NRGBA{R: 0, G: 180, B: 0, A: 255}
+	text := canvas.NewText("Text", green)
 	text.TextStyle.Bold = true
 	myCanvas.SetContent(text)
 	go changeContent(myCanvas)
@@ -27,15 +28,17 @@ func main() {
 func changeContent(c fyne.Canvas) {
 	time.Sleep(time.Second * 2)
 
-	c.SetContent(canvas.NewRectangle(color.Black))
+	blue := color.NRGBA{R: 0, G: 0, B: 180, A: 255}
+	c.SetContent(canvas.NewRectangle(blue))
 
 	time.Sleep(time.Second * 2)
-	c.SetContent(canvas.NewLine(color.Gray{0x66}))
+	c.SetContent(canvas.NewLine(color.Gray{Y: 180}))
 
 	time.Sleep(time.Second * 2)
+	red := color.NRGBA{R: 0xff, G: 0x33, B: 0x33, A: 0xff}
 	circle := canvas.NewCircle(color.White)
 	circle.StrokeWidth = 4
-	circle.StrokeColor = color.RGBA{0xff, 0x33, 0x33, 0xff}
+	circle.StrokeColor = red
 	c.SetContent(circle)
 
 	time.Sleep(time.Second * 2)
